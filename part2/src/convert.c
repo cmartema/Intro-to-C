4971fd4464c597a4d62352dc629abc70b79cc29d
Anita Bui-Martinez <adb2221@columbia.edu>
2023-02-08 15:17:28 -0500

    Part 2 with comments

--- part2/src/convert.c
+++ part2/src/convert.c
@@ -3,18 +3,40 @@
 
 int main() {
 
+	// initiate input and array for binary conversion
 	int input; 
+	int bin[32];
+
+	//putc('1', stdout); 
+	
+	//take in input 
 	printf("Enter an integer : ");
 	scanf("%d", &input); 
 	printf("\n"); 
 
-	// convert var input to binary
-	
+	//print signed and unsigned decimal and hex using printf
 	printf("signed decimal   : %d\n", input);
-	printf("unsigned decimal : %u\n", (unsigned int)input);
+	printf("unsigned decimal : %u\n", input);
 	printf("hexadecimal      : %x\n", input);
-	//printf("binary           : ");
-	//putc('1', stdout); //this will put the binary answer to stdout
-	
+
+	//convert var input to binary 
+	int index = 31; 
+
+	while(index >= 0){
+		
+		bin[index] = input & 1;
+		index--; 
+		input >>= 1; 
+	}
+
+	//print with putc and for loop 
+	printf("binary           : ");
+
+	for(int i = 0; i < 31; i++){
+		putc('%c', bin[i], stdout); 
+		//printf("%d", bin[i]);
+	}
+	printf("\n");
+
 	return EXIT_SUCCESS;
 }
