df28043fb2afb5c8b2bd379fc8be982f0695df02
Anita Bui-Martinez <adb2221@columbia.edu>
2023-02-08 11:47:09 -0500

    Part 2 make and skeleton code

--- part2/src/convert.c
+++ part2/src/convert.c
@@ -2,5 +2,17 @@
 #include <stdlib.h>
 
 int main() {
-    return EXIT_SUCCESS;
+
+	printf("Enter an integer : ");
+	scanf("%d:, &input); 
+
+	//conversion aha 
+	
+	printf("signed decimal   : %d\n", something);
+	printf("unsigned decimal : %d\n", something);
+	printf("hexadecimal      : %d\n", something);
+	printf("binary           : ");
+	putc('1', stdout); //this will put the binary answer to stdout
+	
+	return EXIT_SUCCESS;
 }
